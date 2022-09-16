847794d1457e29c84a2142642c0d2dfe1aa92500
RonanSynnottArm <Ronan.Synnott@arm.com>
2022-09-16 13:36:17 +0900

    added .cprj to Demonstration

---
+++ Projects/B-U585I-IOT02A/Demonstrations/IOT_HTTP_WebServer/MDK-ARM/RTE/_STM32CubeU5_Demo_U585I-IOT02A/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32CubeU5_Demo_U585I-IOT02A' 
+ * Target:  'STM32CubeU5_Demo_U585I-IOT02A' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32u5xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
